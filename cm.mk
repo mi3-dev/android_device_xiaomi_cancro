6674d7bb0350aabcb79b02c1420da3bbc27507fd
秋叶随风ivan <yanziily@gmail.com>
2014-08-05 07:33:16 +0800

    fix build
    Signed-off-by: 秋叶随风ivan <yanziily@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,5 +1,16 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
@@ -8,8 +19,11 @@ TARGET_SCREEN_WIDTH := 1080
 # Release name
 PRODUCT_RELEASE_NAME := cancro
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/cancro/device_cancro.mk)
 
 # Inherit some common CM stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
@@ -17,8 +31,8 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/xiaomi/cancro/device_cancro.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cancro
