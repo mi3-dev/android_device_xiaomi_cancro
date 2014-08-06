be4d1c9240e56d5928f6f4be002aeacbd527ae28
秋叶随风ivan <yanziily@gmail.com>
2014-08-07 00:10:03 +0800

    fix link ko modules
    Signed-off-by: 秋叶随风ivan <yanziily@gmail.com>

--- kernel/Android.mk
+++ kernel/Android.mk
@@ -3,7 +3,7 @@ ifneq ($(filter cancro,$(TARGET_DEVICE)),)
 
 #Create symbolic link
 $(shell mkdir -p $(TARGET_OUT)/lib/modules; \
-        ln -sf /system/lib/modules/$(WLAN_CHIPSET)/pronto_wlan.ko \
+        ln -sf /system/lib/modules/pronto/pronto_wlan.ko \
                $(TARGET_OUT)/lib/modules/wlan.ko)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
