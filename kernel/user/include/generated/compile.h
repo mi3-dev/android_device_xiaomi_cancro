b35d3ef31b804be18be3328112d77500ad31ea37
huangqiwu <huangqiwu@xiaomi.com>
2014-08-07 12:14:28 +0800

    Update kernel
    Signed-off-by: huangqiwu <huangqiwu@xiaomi.com>

--- kernel/user/include/generated/compile.h
+++ kernel/user/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Aug 5 12:32:27 CST 2014"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Aug 6 21:28:34 CST 2014"
 #define LINUX_COMPILE_BY "builder"
 #define LINUX_COMPILE_HOST "taishan"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- kernel/user/include/generated/utsrelease.h
+++ kernel/user/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-gbb551a9-01014-gd01aa0f"
+#define UTS_RELEASE "3.4.0-gbb551a9-01020-g385bd2d"
