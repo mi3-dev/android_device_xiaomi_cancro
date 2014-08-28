648f429079ad1a46f6994e7dda2c75e1196ad2e3
huangqiwu <huangqiwu@xiaomi.com>
2014-08-28 16:43:17 +0800

    cancro: Add radio images
     - from miui 4.8.27
    
    Signed-off-by: huangqiwu <huangqiwu@xiaomi.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,5 +4,25 @@ include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
+#----------------------------------------------------------------------
+# Radio image
+#----------------------------------------------------------------------
+ifeq ($(ADD_RADIO_FILES), true)
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+    $(call add-radio-file,radio/$(f)))
+endif
+
+TARGET_BOOTLOADER_EMMC_INTERNAL := device/xiaomi/cancro/images/emmc_appsboot.mbn
+$(TARGET_BOOTLOADER_EMMC_INTERNAL): $(TARGET_BOOTLOADER)
+
+INSTALLED_RADIOIMAGE_TARGET += $(TARGET_BOOTLOADER_EMMC_INTERNAL)
+$(call add-radio-file,images/NON-HLOS.bin)
+$(call add-radio-file,images/rpm.mbn)
+$(call add-radio-file,images/sbl1.mbn)
+$(call add-radio-file,images/sdi.mbn)
+$(call add-radio-file,images/tz.mbn)
+
 # include the non-open-source counterpart to this file
 -include vendor/xiaomi/cancro/AndroidBoardVendor.mk
---
+++ releasetools.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Emit commands needed for HTC devices during OTA installation
+(installing the aboot/modem/rpm/sbl/tz images)."""
+
+import common
+import re
+import sha
+
+def FullOTA_Assertions(info):
+  print "FullOTA_Assertions not implemented"
+
+def IncrementalOTA_Assertions(info):
+  print "IncrementalOTA_Assertions not implemented"
+
+def InstallImage(img_name, img_file, partition, info):
+  common.ZipWriteStr(info.output_zip, img_name, img_file)
+  if partition == "modem":
+    info.script.AppendExtra(('unmount("/vendor/firmware");'))
+  info.script.AppendExtra(('package_extract_file("' + img_name + '", "/dev/block/platform/msm_sdcc.1/by-name/' + partition + '");'))
+
+image_partitions = {
+   'emmc_appsboot.mbn' : 'aboot',
+   'NON-HLOS.bin'      : 'modem',
+   'rpm.mbn'           : 'rpm',
+   'sbl1.mbn'          : 'sbl1',
+   'sdi.mbn'           : 'dbi',
+   'tz.mbn'            : 'tz'
+}
+
+def FullOTA_InstallEnd(info):
+  for k, v in image_partitions.iteritems():
+    try:
+      img_file = info.input_zip.read("RADIO/" + k)
+      info.script.Print("Writing image " + k + "...")
+      InstallImage(k, img_file, v, info)
+    except KeyError:
+      print "warning: no " + k + " image in input target_files; not flashing " + k
+
+
+def IncrementalOTA_InstallEnd(info):
+  for k, v in image_partitions.iteritems():
+    try:
+      source_file = info.source_zip.read("RADIO/" + k)
+      target_file = info.target_zip.read("RADIO/" + k)
+      if source_file != target_file:
+        InstallImage(k, target_file, v, info)
+      else:
+        print k + " image unchanged; skipping"
+    except KeyError:
+      print "warning: " + k + " image missing from target; not flashing " + k
