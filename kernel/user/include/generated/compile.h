e81975ad3ef7ab1ac745e11a090823a57fc8d4bb
huangqiwu <huangqiwu@xiaomi.com>
2014-08-29 10:00:25 +0800

    cancro: Update binary kernel 20140827
    Signed-off-by: huangqiwu <huangqiwu@xiaomi.com>

--- kernel/user/include/generated/compile.h
+++ kernel/user/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Mon Aug 25 20:14:35 CST 2014"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Aug 27 20:17:48 CST 2014"
 #define LINUX_COMPILE_BY "builder"
 #define LINUX_COMPILE_HOST "taishan"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- kernel/user/include/generated/utsrelease.h
+++ kernel/user/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-gbb551a9-01054-gd509b4f"
+#define UTS_RELEASE "3.4.0-gbb551a9-01059-g6de04aa"
